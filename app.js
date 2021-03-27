caabc0729dc8327ae72e81b38cf773bbe3b7e2af
Oleg <pirr31@mail.ru>
2021-03-27 17:58:04 +0300

    fix: remove the use of tag selectors according to rules 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton=document.getElementsByClassName("main-btn")[0];//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -32,19 +32,23 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="list-item";
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='task list-item-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className='input-checkbox';
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task input-text list-item-input-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit main-btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete main-btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="remove-icon";
     deleteButton.appendChild(deleteButtonImg);
 
 
